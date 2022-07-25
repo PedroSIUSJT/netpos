326c9164d49e9f51b9cfc57a3bd1c6e96dc9896b
PedroDevJava <40042900+PedroSIUSJT@users.noreply.github.com>
2022-07-25 09:40:06 -0300

    Add files via upload

---
+++ src/main/java/br/com/netpos/gateway/h2/ProductRepository.java
@@ -0,0 +1,12 @@
+package br.com.netpos.gateway.h2;
+
+
+import br.com.netpos.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByNameStartingWith(String name);
+//    Product findByProduct(Long code);
+}
---
+++ src/test/java/br/com/netpos/NetposServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.netpos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NetposServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
