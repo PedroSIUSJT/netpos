aa2dc993c27684b9914bd7a6e529712fbdf27a0d
PedroDevJava <40042900+PedroSIUSJT@users.noreply.github.com>
2022-07-25 09:35:41 -0300

    Add files via upload
    Ajuste Swagger endpoint retirado requestMapping.

---
+++ netpos/netpos/src/main/java/br/com/netpos/NetposServiceApplication.java
@@ -0,0 +1,16 @@
+package br.com.netpos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@SpringBootApplication
+@EnableSwagger2
+public class NetposServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetposServiceApplication.class, args);
+	}
+
+}
