f43b89d5a26ae06314c9f3ef68b3cb0b1980b8c2
PedroDevJava <40042900+PedroSIUSJT@users.noreply.github.com>
2022-07-25 13:33:16 -0300

    Delete src directory

--- src/main/java/br/com/netpos/gateway/h2/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.netpos.gateway.h2;
-
-
-import br.com.netpos.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    
-}
